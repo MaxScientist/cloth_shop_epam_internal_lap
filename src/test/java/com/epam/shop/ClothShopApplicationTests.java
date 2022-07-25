dca012e90e61941cc89dd2d539e9400d707daabb
MaxScientist <maksatgalymzhan@gmail.com>
2022-07-25 12:22:00 +0600

    Test

--- src/test/java/com/epam/shop/ClothShopApplicationTests.java
+++ src/test/java/com/epam/shop/ClothShopApplicationTests.java
@@ -1,10 +1,32 @@
 package com.epam.shop;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@EnableAutoConfiguration
 class ClothShopApplicationTests {
+
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public MockMvc mockMvc() {
+        return MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
 
     @Test
     void contextLoads() {
--- src/test/java/com/epam/shop/controller/CategoryControllerTest.java
+++ src/test/java/com/epam/shop/controller/CategoryControllerTest.java
@@ -2,31 +2,34 @@ package com.epam.shop.controller;
 
 
 import com.epam.shop.dto.CategoryDTO;
-import com.epam.shop.mapper.DTOMapper;
 import com.epam.shop.dto.ProductDTO;
 import com.epam.shop.entity.Category;
 import com.epam.shop.entity.Product;
+import com.epam.shop.mapper.DTOMapper;
 import com.epam.shop.service.impl.CategoryServiceImpl;
+import com.epam.shop.service.impl.ProductServiceImpl;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Transactional
 public class CategoryControllerTest {
@@ -44,6 +47,12 @@ public class CategoryControllerTest {
     @Autowired
     private DTOMapper<Category, CategoryDTO> categoryMapper;
 
+    @Autowired
+    private DTOMapper<Product, ProductDTO> productMapper;
+
+    @Autowired
+    private ProductServiceImpl productService;
+
     @Test
     void findAllCategories() throws Exception {
         createCategories();
@@ -57,12 +66,99 @@ public class CategoryControllerTest {
                 .getResponse()
                 .getContentAsString();
 
-        List<CategoryDTO> categoryDTOS = objectMapper.readValue(httpResponse, new TypeReference<>(){});
-        List<Category> categories =  categoryDTOS.stream().
+        List<CategoryDTO> categoryDTOS = objectMapper.readValue(httpResponse, new TypeReference<>() {
+        });
+
+        List<Category> categories = categoryDTOS.stream().
                 map(categoryMapper::fromDTO).collect(toList());
 
         assertEquals(fromDB.toString(), categories.toString());
     }
+
+    @Test
+    void createAndSaveCategory() throws Exception {
+        Category categoryToSaveToDB = createCategory();
+        String requestBody = objectMapper.writeValueAsString(categoryMapper.toDTO(categoryToSaveToDB));
+
+        String httpResponse = mockMvc.perform(post("/api/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        CategoryDTO categoryDTO = objectMapper.readValue(httpResponse, new TypeReference<>() {
+        });
+        Category category = categoryMapper.fromDTO(categoryDTO);
+        Category fromDB = categoryService.findById(category.getId());
+
+        assertEquals(fromDB.toString(), category.toString());
+    }
+
+    @Test
+    void findCategoryViaId() throws Exception {
+        Category categoryToSaveToDB = createCategory();
+
+        Category categorySavedVersion = categoryService.save(categoryToSaveToDB);
+
+        String httpResponse = mockMvc.perform(get("/api/categories/" + categorySavedVersion.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        CategoryDTO categoryDTO = objectMapper.readValue(httpResponse, new TypeReference<>() {
+        });
+        Category categoryFromDTO = categoryMapper.fromDTO(categoryDTO);
+
+        assertEquals(categorySavedVersion.toString(), categoryFromDTO.toString());
+    }
+
+    @Test
+    void findProductsByCategoryId() throws Exception {
+        List<Product> savedProducts = createProducts();
+
+        String httpResponse = mockMvc.perform(get("/api/categories/"+savedProducts.get(0).getCategoryId()+"/products"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        List<ProductDTO> productDTOS = objectMapper.readValue(httpResponse, new TypeReference<>() {});
+        List<Product> productList = productDTOS.stream().map(productMapper::fromDTO).collect(toList());
+
+        assertFalse(productList.isEmpty());
+        assertEquals(savedProducts.toString(), productList.toString());
+    }
+
+    private List<Product> createProducts() {
+        Category categoryToSave = createCategory();
+        int categoryId = categoryService.save(categoryToSave).getId();
+        List<Product> products = new ArrayList<>();
+        for (int i = 1; i < 6; i++) {
+            Product product = new Product();
+            product.setProductName(i + "product");
+            product.setPrice(i*1000);
+            product.setQuantity(i*10);
+            product.setCategoryId(categoryId);
+
+            Product savedProduct = productService.save(product);
+            products.add(savedProduct);
+        }
+        return products;
+    }
+
+    Category createCategory() {
+        Category category = new Category();
+        category.setName("1st Category");
+        category.setProducts(new ArrayList<>());
+        return category;
+    }
+
 
     private void createCategories() {
         IntStream.range(0, 5).forEachOrdered(i -> {
